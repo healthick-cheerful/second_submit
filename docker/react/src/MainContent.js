d4b94530a303ee39f2b243fa9879adb64b471400
healthick <healthick.cheerful@gmail.com>
2022-11-26 14:59:51 +0900

    design ModeSelector

--- docker/react/src/MainContent.js
+++ docker/react/src/MainContent.js
@@ -1,10 +1,12 @@
 import React from "react"
+import ModeSelector from "./ModeSelector"
 import "./css/MainContent.css"
 
 class MainContent extends React.Component {
     render() {
         return (
             <div className="main-content">
+                <ModeSelector />
             </div>
         )
     }
