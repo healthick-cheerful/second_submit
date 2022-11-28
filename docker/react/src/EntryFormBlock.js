8f6905d6b1e48d71058c57a4ae491c79947adc7f
root <root@DESKTOP-6PB9B96.localdomain>
2022-11-29 00:58:21 +0900

    design EntryFormBlock

--- docker/react/src/EntryFormBlock.js
+++ docker/react/src/EntryFormBlock.js
@@ -5,6 +5,14 @@ class EntryFormBlock extends React.Component {
     render() {
         return (
             <div className="entry-form-block">
+                <form method="post">
+                    <div className="text-input">
+                        <textarea></textarea>
+                    </div>
+                    <div className="action-bar">
+                        <button>Confirm</button>
+                    </div>
+                </form>
             </div>
         )
     }
