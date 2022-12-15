afcb08c903c63d7258b0c6924037e63d1f7d4000
healthick <healthick.cheerful@gmail.com>
2022-12-15 15:09:04 +0900

    add function of updating timeline in follow_timeline

--- docker/php/public/get_follow_entries.php
+++ docker/php/public/get_follow_entries.php
@@ -11,49 +11,94 @@ $result_array = [
 if(isset($_SESSION['login_user_id'])) {
     try {
         $dbh = Db::getHandle();
-        $select_sql = '
-            SELECT
-                bbs_entries.id AS id,
-                bbs_entries.user_id,
-                bbs_entries.body,
-                bbs_entries.created_at,
-                users.name AS user_name,
-                users.icon_filename
-            FROM
-                `bbs_entries`
-            INNER JOIN
-                `users`
-            ON
-                bbs_entries.user_id = users.id
-            WHERE
-                bbs_entries.user_id
-            IN
-                (
-                    SELECT
-                        `followed_user_id`
-                    FROM
-                        `user_relationship`
-                    WHERE
-                        `follower_user_id` = :login_user_id_first
-                )
-            OR
-                bbs_entries.user_id = :login_user_id_second
-            ORDER BY
-                bbs_entries.id
-            DESC
-            LIMIT
-                10
-        ;';
-        $select_sth = $dbh->prepare($select_sql);
-        // この環境ではエミュレート無効にしているためbindValueは2つ必要
-        $select_sth->bindValue(':login_user_id_first', $_SESSION['login_user_id']);
-        $select_sth->bindValue(':login_user_id_second', $_SESSION['login_user_id']);
+        if(isset($_POST['last_id'])) {
+            $select_sql = '
+                SELECT
+                    bbs_entries.id AS id,
+                    bbs_entries.user_id,
+                    bbs_entries.body,
+                    bbs_entries.created_at,
+                    users.name AS user_name,
+                    users.icon_filename
+                FROM
+                    `bbs_entries`
+                INNER JOIN
+                    `users`
+                ON
+                    bbs_entries.user_id = users.id
+                WHERE
+                    bbs_entries.id < :last_id
+                AND
+                    (
+                        bbs_entries.user_id
+                        IN
+                            (
+                                SELECT
+                                    `followed_user_id`
+                                FROM
+                                    `user_relationship`
+                                WHERE
+                                    `follower_user_id` = :login_user_id_first
+                            )
+                        OR
+                            bbs_entries.user_id = :login_user_id_second
+                    )
+                ORDER BY
+                    bbs_entries.id
+                DESC
+                LIMIT
+                    10
+            ;';
+            $select_sth = $dbh->prepare($select_sql);
+            // この環境ではエミュレート無効にしているためbindValueは2つ必要
+            $select_sth->bindValue(':login_user_id_first', $_SESSION['login_user_id']);
+            $select_sth->bindValue(':login_user_id_second', $_SESSION['login_user_id']);
+            $select_sth->bindValue(':last_id', $_POST['last_id']);
+        } else {
+            $select_sql = '
+                SELECT
+                    bbs_entries.id AS id,
+                    bbs_entries.user_id,
+                    bbs_entries.body,
+                    bbs_entries.created_at,
+                    users.name AS user_name,
+                    users.icon_filename
+                FROM
+                    `bbs_entries`
+                INNER JOIN
+                    `users`
+                ON
+                    bbs_entries.user_id = users.id
+                WHERE
+                    bbs_entries.user_id
+                IN
+                    (
+                        SELECT
+                            `followed_user_id`
+                        FROM
+                            `user_relationship`
+                        WHERE
+                            `follower_user_id` = :login_user_id_first
+                    )
+                OR
+                    bbs_entries.user_id = :login_user_id_second
+                ORDER BY
+                    bbs_entries.id
+                DESC
+                LIMIT
+                    10
+            ;';
+            $select_sth = $dbh->prepare($select_sql);
+            // この環境ではエミュレート無効にしているためbindValueは2つ必要
+            $select_sth->bindValue(':login_user_id_first', $_SESSION['login_user_id']);
+            $select_sth->bindValue(':login_user_id_second', $_SESSION['login_user_id']);
+        }
         $select_sth->execute();
-        $entries_data = $select_sth->fetchAll() ?? [];
-        $left_id = $entries_data[array_key_last($entries_data)]["id"] ?? "";
-        $right_id = $entries_data[array_key_first($entries_data)]["id"] ?? "";
-        if($left_id !== "") {
-                $select_sql = '
+        $entries_data = $select_sth->fetchAll();
+        if($entries_data !== []) {
+            $left_id = $entries_data[array_key_last($entries_data)]["id"] ?? "";
+            $right_id = $entries_data[array_key_first($entries_data)]["id"] ?? "";
+            $select_sql = '
                 SELECT
                     *
                 FROM
@@ -69,8 +114,13 @@ if(isset($_SESSION['login_user_id'])) {
             $select_sth->bindValue(':left_id', $left_id);
             $select_sth->bindValue(':right_id', $right_id);
             $select_sth->execute();
-            $images_data = $select_sth->fetchAll() ?? [];
+            $images_data = $select_sth->fetchAll();
+            $result_array["last_id"] = $left_id;
+        } else {
+            $images_data = [];
+            $result_array["left_id"] = false;
         }
+
     } catch(\Throwable $e) {
         $result_array['server_error'] = true;
         echo $e;
